c6e1ddabc45e6fe64ed7be64171e823fcf05dcb2
Wim Schmitz <wim@schmitz.cc>
2021-04-21 21:35:51 +0200

    add code

---
+++ app.js
@@ -0,0 +1,106 @@
+var Paynl = require('paynl-sdk');
+var express = require('express');
+
+Paynl.Config.setApiToken('609f82277a2afab05e845011ebf57e05b1c11aca');
+Paynl.Config.setServiceId('SL-9540-4851'); 
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const app = express();
+
+app.get('/startTransactionTest',function (req, res){
+
+  Paynl.Transaction.start({
+    //the amount in euro
+    amount: 1,
+
+    testMode: true,
+    
+    //we redirect the user back to this url after the payment
+    returnUrl: "http://www.google.be",
+    
+    //the ip address of the user
+    ipAddress: '81.164.178.176' 
+  })
+  .subscribe(
+    function (result) {
+      console.log(result)
+      //redirect the user to this url to complete the payment
+      console.log(result.paymentURL); 
+      
+      // the transactionId, use this to fetch the transaction later
+      console.log(result.transactionId);
+
+      res.redirect(result.paymentURL)
+    }, 
+    function (error) {
+      console.error(error); 
+    }
+  );
+})
+
+app.get('/startTransaction',function (req, res){
+
+  Paynl.Transaction.start({
+    //the amount in euro
+    amount: 1,
+    
+    //we redirect the user back to this url after the payment
+    returnUrl: "http://www.google.be",
+    
+    //the ip address of the user
+    ipAddress: '81.164.178.176' 
+  })
+  .subscribe(
+    function (result) {
+      console.log(result)
+      //redirect the user to this url to complete the payment
+      console.log(result.paymentURL); 
+      
+      // the transactionId, use this to fetch the transaction later
+      console.log(result.transactionId);
+
+      res.redirect(result.paymentURL)
+    }, 
+    function (error) {
+      console.error(error); 
+    }
+  );
+})
+
+
+app.post('/startTransaction',function (req, res){
+  res.send("We will redirect you to the payment resolver soon, please stand by!")
+  Paynl.Transaction.start({
+    //the amount in euro
+    amount: req.data,
+
+    testMode: true,
+    
+    //we redirect the user back to this url after the payment
+    returnUrl: "http://www.google.be",
+    
+    //the ip address of the user
+    ipAddress: '81.164.178.176' 
+  })
+  .subscribe(
+    function (result) {
+      console.log(result)
+      //redirect the user to this url to complete the payment
+      console.log(result.paymentURL); 
+      
+      // the transactionId, use this to fetch the transaction later
+      console.log(result.transactionId);
+
+      res.redirect(result.paymentURL)
+    }, 
+    function (error) {
+      console.error(error); 
+    }
+  );
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
