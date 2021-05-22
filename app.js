aebe1ef32164f1428e192e1fd3e2f27ebca8d6c4
Wim Schmitz <wim@schmitz.cc>
2021-05-22 04:58:33 +0200

    CORS Try 4

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var transactionsRouter = require('./routes/transactions');
 
 var app = express();
 
-app.use(function(req, res, next) {
+app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://wimschmitz.github.io"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
