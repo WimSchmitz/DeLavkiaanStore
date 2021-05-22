43fec10a34eab262ce6992ebaebb48cbe363d823
Wim Schmitz <wim@schmitz.cc>
2021-05-22 04:06:02 +0200

    Enable CORS Try 1

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var transactionsRouter = require('./routes/transactions');
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://wimschmitz.github.io/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
